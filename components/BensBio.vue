87eb29d5b9a3ef779ee3a34f193dc6e81be1f6ff
1994benc <43471788+1994benc@users.noreply.github.com>
2022-12-10 09:52:19 +0000

    Add bio

---
+++ components/BensBio.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col gap-4">
+    <div>
+      Ben Chomsang is a full-stack software wizard currently working at Zinc,
+      where he uses his magical coding powers to automate background checking
+      for growing companies.
+    </div>
+
+    <div>
+      In his free time, Ben enjoys spending time with his 3-year-old border
+      collie, taking him on long walks in the field or going on several-hour hikes. He
+      also has a passion for gardening and tends to his beloved collection of
+      houseplants, which he talks to and sings to in order to help them grow.
+    </div>
+
+    <div>
+      When he's not busy with his plants or his dog, you can find him conjuring
+      up new spells and incantations to use in his software engineering work.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div
-    class="mx-auto max-w-5xl h-full flex flex-col justify-center items-center p-4 gap-4"
+    class="mx-auto max-w-5xl h-full flex flex-col justify-center items-center p-4 gap-6"
   >
+    <div class="max-w-md my-6"><BensBio /></div>
     <div class="text-sm mb-1">Here's a random emoji</div>
-      <EmojiOfTheDay />
+    <EmojiOfTheDay />
     <div
-      class="flex justify-start items-center gap-5 my-4 border rounded px-4 py-2 border-gray-600 dark:border-opacity-100 border-opacity-25 "
+      class="flex justify-start items-center gap-5 my-4 border rounded px-4 py-2 border-gray-600 dark:border-opacity-100 border-opacity-25"
     >
       <NuxtLink
         href="https://github.com/bencsn"
